d40a6dd5adeb81088c6a9d4e394f45ba2dd30a6e
Bissentayev, Darkhan AtyrauTechInvest LLP <darkhanbissentayev@tengizchevroil.com>
2021-05-25 19:14:06 +0500

    added dal, dbcontext. Need to set test connection

---
+++ BAF.DataAccess.MSSQL/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BAF.DataAccess.MSSQL
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddDataAccessSqlServer(this IServiceCollection services, string connectionString)
+        {
+            services
+                .AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connectionString));
+            services.AddScoped<ApplicationDbContext>();
+            return services;
+        }
+    }
+}
